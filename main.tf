1cf3c5ad969eff29c6bb8de09c0c3939a3046e4e
ryichk <ryichk@gmail.com>
2021-09-10 05:41:25 +0900

    :sparkles: add IAM Role module

---
+++ main.tf
@@ -0,0 +1,56 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect = "Allow"
+    actions = ["ec2:DescribeRegions"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "example" {
+  name = "example"
+  policy = data.aws_iam_policy_document.allow_describe_regions.json
+}
+
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "example" {
+  name = "example"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "example" {
+  role = aws_iam_role.example.name
+  policy_arn = aws_iam_policy.example.arn
+}
+
+module "describe_regions_for_ec2" {
+  source = "./iam_role"
+  name = "describe-regions-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy = data.aws_iam_policy_document.allow_describe_regions.json
+}
+
+data "aws_iam_policy_document" "alb" {
+  statement {
+    effect = "Allow"
+    actions = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
+    principals {
+      type = "AWS"
+      identifiers = ["582318560864"]
+    }
+  }
+}
