51f838c02bdc77d54001319030189489639acabe
ryichk <ryichk@gmail.com>
2021-09-16 04:20:21 +0900

    :sparkles: Added ECS Scheduled Tasks setting and CloudWatch logs

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -2,3 +2,14 @@ resource "aws_cloudwatch_log_group" "for_ecs" {
   name = "/ecs/example"
   retention_in_days = 180
 }
+
+resource "aws_cloudwatch_log_group" "for_ecs_scheduled_tasks" {
+  name = "/ecs-scheduled-tasks/example"
+  retention_in_days = 180
+}
+
+resource "aws_cloudwatch_event_rule" "example_batch" {
+  name = "example_batch"
+  description = "very important batch"
+  schedule_expression = "cron(*/2 * * * ? *)"
+}
